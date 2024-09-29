25b148f2303fa4bdd652ef994bc3cadca5ca4612
evan-corpusant <evan@corpusant.ai>
2024-09-29 09:26:56 -0700

    login improvement

--- nextapp/app/login/page.tsx
+++ nextapp/app/login/page.tsx
@@ -13,8 +13,7 @@ export default function Login() {
   const router = useRouter();
   const supabase = createClientComponentClient();
 
-  const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSignUp = async () => {
     setError(null);
     try {
       const { error } = await supabase.auth.signUp({
@@ -139,6 +138,7 @@ export default function Login() {
             <div className="mt-6">
               <button
                 onClick={handleSignUp}
+                type="button"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Sign up
