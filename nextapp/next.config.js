89eee0591739adcd830672d4ce27a1da5c06f23e
evan-corpusant <evan@corpusant.ai>
2024-09-29 09:02:57 -0700

    Add environment variable logging and next.config.js

--- nextapp/next.config.js
+++ nextapp/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['localhost'],
-    unoptimized: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    NEXT_PUBLIC_ODDS_API_KEY: process.env.NEXT_PUBLIC_ODDS_API_KEY,
   },
 }
 
