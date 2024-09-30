ad30a1f0c32e06720838b9175bd6296d55898be9
evan-corpusant <evan@corpusant.ai>
2024-09-29 19:28:19 -0700

    build please

--- nextapp/lib/supabase.ts
+++ nextapp/lib/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import { getBaseUrl } from "@/utils/environment";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
@@ -9,6 +8,5 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,
-    redirectTo: `${getBaseUrl()}/auth/callback`,
   },
 });
