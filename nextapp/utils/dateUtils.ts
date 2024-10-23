7cc9f4a1aa08ab1a444be3a1981bf50a2e17d693
evan-corpusant <evan@corpusant.ai>
2024-10-23 09:06:08 -0700

    week 8

--- nextapp/utils/dateUtils.ts
+++ nextapp/utils/dateUtils.ts
@@ -2,12 +2,12 @@ import { Game } from "@/types";
 
 export function calculateNFLWeek(date: Date): number {
   // For now, always return Week 6
-  return 7;
+  return 8;
 }
 
 export function getCurrentNFLWeek(): number {
   // Manually set to Week 6
-  return 7;
+  return 8;
 }
 
 export function getWeekStartDate(week: number): Date {
